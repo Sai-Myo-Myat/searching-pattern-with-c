3789c1fbc4ec9e9898e6b1251d72d977de37d7ab
Sai Myo Myat <saimyomyat511@gmail.com>
2022-12-05 13:25:45 +0630

    updated

--- main.cpp
+++ main.cpp
@@ -1,48 +1,77 @@
 
+#include "cstdio"
 #include "stdio.h"
 
 
 int calculateSizeOfArr(char arr[]);
-void searching_pattern(char expression[]);
+bool searching_pattern(char expression[]);
 
 int main() {
     char expression[100] ;
 
     printf("Enter word to find: ");
     scanf(" %[^\n]",&expression);
-    printf("prgram start!!!!");
-    searching_pattern(expression);
-
-    printf("finished");
+    puts(expression);
+    int sizeOfArray = calculateSizeOfArr(expression);
+    printf("program start!!!!'\n");
+    for (int i = 0; i < sizeOfArray; ++i) {
+        printf("expression char is %c \n",expression[i]);
+    }
+    bool result = searching_pattern(expression);
+        if (result == true) {
+            printf("We found it!");
+        }else   {
+            printf("We can't found!");
+        }
 
     return 0;
 }
 
-void searching_pattern(char expression[]) {
-    printf("searching start");
+bool searching_pattern(char expression[]) {
+    printf("searching start\n");
     FILE *fptr;
-    int sizeOfArray = calculateSizeOfArr(expression);
-    char currentChar = fgetc(fptr);
-    while (!feof(fptr)){
 
-        if (currentChar == expression[0]){
-            for (int i = 0; i < sizeOfArray; ++i) {
-                char nextChar = fgetc(fptr);
-                if (expression[i] == nextChar){
-                    printf("fond it");
-                }else {
-                    printf("word not found");
+    fptr = fopen("text.txt", "r");
+    bool isIt = false;
+    int currentChar = fgetc(fptr);
+
+    int sizeOfArray = calculateSizeOfArr(expression);
+    printf("size %d\n",sizeOfArray);
+
+    int expressionFirstChar = expression[0];
+
+    if (fptr != NULL){
+        while (!feof(fptr)){
+            if (currentChar == expressionFirstChar){
+                for (int i = 1; i < sizeOfArray; ++i) {
+                    printf("num of i %d size of array %d \n", i,sizeOfArray);
+
+                    currentChar = fgetc(fptr);
+                    int expressionNextChar = expression[i];
+                    if (expressionNextChar == currentChar){
+                        printf("next value %d vs expressiosn value %d\n", currentChar,expressionNextChar);
+                        isIt = true;
+                    } else {
+                        isIt = false;
+                        break;
+                    }
                 }
             }
+            currentChar = getc(fptr);
         }
-        fgetc(fptr);
+    }else {
+        printf("file not found!");
     }
+
     fclose(fptr);
+
+    return  isIt;
+
 }
 
 int calculateSizeOfArr(char arr[]){
     int sizeOf = 0;
-    if (arr[sizeOf] != '\0'){
+    while (arr[sizeOf] != '\0'){
         sizeOf++;
     }
 
